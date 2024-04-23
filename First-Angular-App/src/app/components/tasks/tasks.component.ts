cf873466e0873ee3b10d46161b3f3fc4218a6953
DSutapa <sutapadutta.november@gmail.com>
2024-04-23 18:02:59 +0530

    task, task item and fa icon added

---
+++ First-Angular-App/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {Task} from '../../Task';
+import {TASKS} from '../../mock-task';
+import { CommonModule } from '@angular/common';
+import { TaskItemComponent } from "../task-item/task-item.component";
+
+@Component({
+    selector: 'app-tasks',
+    standalone: true,
+    templateUrl: './tasks.component.html',
+    styleUrl: './tasks.component.css',
+    imports: [CommonModule, TaskItemComponent]
+})
+export class TasksComponent {
+  tasks: Task[] = TASKS;
+}
